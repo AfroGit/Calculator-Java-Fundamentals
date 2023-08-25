e82ac8c647ef5166c74c5847d81e79d4dddc1a6b
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-08-25 23:41:41 +0000

    Know who you are creating code for,this means to adopt to more clear concise and relevant names for variables and output messages.

--- GrossPayCalculator.java
+++ GrossPayCalculator.java
@@ -6,15 +6,27 @@ public class GrossPayCalculator{
 
   public static void main(String arg[]){
     // 1. Get the amount of hours worked.
+    
     System.out.println("Enter the number of hours the employee worked.");
     Scanner scanner = new Scanner(System.in);//checks input
-    int hours = scanner.nextInt();//assigns/validates input value to a variable 
+    int hours = scanner.nextInt();
+    //Above assigns/validates input value to a variable 
 
     // 2. Get the hourly pay rate.
+    
+    System.out.println("Enter the hourly pat rate.");
+    
+    double payRate = scanner.nextDouble();//double commonly used for float.
+    
+    scanner.close();//scanner closed to create room
 
     // 3. Multiply the hours and pay rate.
 
+    double grossPay = hours * payRate;//use operator to calculate
+
     // 4. Display results.
+
+    System.out.println("The employee's gross pay is $" + grossPay);
     
   }
   
