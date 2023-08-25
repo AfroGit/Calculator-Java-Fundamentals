d8b017aa91207102b7b9327634703bc8ee521c5c
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-08-25 19:33:29 +0000

    Initial commit

---
+++ GrossPayCalculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//capitalize class name below.
+
+public class GrossPayCalculator{
+
+  public static void main(String arg[]){
+    // 1. Get the amount of hours worked.
+    System.out.println("Enter the number of hours the employee worked.");
+
+    // 2. Get the hourly pay rate.
+
+    // 3. Multiply the hours and pay rate.
+
+    // 4. Display results.
+    
+  }
+  
+}
